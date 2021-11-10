d265d703d2ec27949bc21c5aa1c2e287c248a2e5
IPlayZed <pauljouser@gmail.com>
2021-11-10 21:10:52 +0100

    Added SPI related projects.

---
+++ 09/README.md
@@ -0,0 +1,3 @@
+# SPI communications
+
+In these examples SPI is used to output number to the 4-digit display. In earlier examples we saw that this can be done via plainly configuring ports as the output of the MCU, however it requires more software during operation, however SPI configured and connected properly can ease this task.
---
+++ 09/f1/Keil 8051 v9.60.0 - Debug/src/subdir.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+A51_UPPER_SRCS += \
+../src/SILABS_STARTUP.A51 
+
+C_SRCS += \
+../src/InitDevice.c \
+../src/f1_main.c 
+
+OBJS += \
+./src/InitDevice.OBJ \
+./src/SILABS_STARTUP.OBJ \
+./src/f1_main.OBJ 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.OBJ: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Compiler'
+	C51 "@$(patsubst %.OBJ,%.__i,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/InitDevice.OBJ: C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/EFM8BB1/inc/SI_EFM8BB1_Register_Enums.h C:/Users/hallgato/SimplicityStudio/v5_workspace/f1/inc/InitDevice.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/EFM8BB1/inc/SI_EFM8BB1_Defs.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/stdbool.h
+
+src/%.OBJ: ../src/%.A51
+	@echo 'Building file: $<'
+	@echo 'Invoking: Keil 8051 Assembler'
+	AX51 "@$(patsubst %.OBJ,%.__ia,$@)" || $(RC)
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/f1_main.OBJ: C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/EFM8BB1/inc/SI_EFM8BB1_Register_Enums.h C:/Users/hallgato/SimplicityStudio/v5_workspace/f1/inc/InitDevice.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/EFM8BB1/inc/SI_EFM8BB1_Defs.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/si_toolchain.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/stdint.h C:/SiliconLabs/SimplicityStudio/v5/developer/sdks/8051/v4.2.1/Device/shared/si8051Base/stdbool.h
+
+
